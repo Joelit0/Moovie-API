fe6c4b2061a91ac3b5745e110ee58fd910b90eca
JoelAlayon123 <joel.alayon@moove-it.com>
2020-04-21 15:09:36 -0300

    Implement genres endpoint

---
+++ spec/controllers/api/genres_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe GenresController, type: :controller do
+  describe "GET #index" do
+    before do
+      genre = create(:genre)
+      get :index
+    end  
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+    it "JSON body response contains expected genre attributes" do
+      json_response = JSON.parse(response.body)
+      expect(json_response.first.keys).to match_array(["id","name", "created_at", "updated_at"])
+    end
+  end
+end
