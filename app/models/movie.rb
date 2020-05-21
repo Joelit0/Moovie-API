8709079389f7dff5d9622c609f9e941ddb286d17
JoelAlayon123 <joel.alayon@moove-it.com>
2020-05-21 14:24:09 -0300

    validates_presence_of with multiple fields

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,6 @@ class Movie < ApplicationRecord
   scope :filter_by_title, -> (title) { where("title like ?", "#{title}%")}
   has_many :videos, dependent: :destroy
   accepts_nested_attributes_for :videos
-  validates_presence_of :title
-  validates_presence_of :tagline
-  validates_presence_of :overview
-  validates_presence_of :release_date
-  validates_presence_of :poster_url
-  validates_presence_of :backdrop_url
-  validates_presence_of :imdb_id
+  validates_presence_of :title, :tagline, :overview, :release_date, :poster_url, :backdrop_url, :imdb_id
 end
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,4 @@
 class Video < ApplicationRecord
   belongs_to :movie
-  validates_presence_of :size
-  validates_presence_of :format
-  validates_presence_of :url
+  validates_presence_of :size, :format, :url
 end
