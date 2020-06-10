2e434c7f1a1428d8d7edf770ccb004538b024feb
JoelAlayon123 <joel.alayon@moove-it.com>
2020-06-10 13:53:35 -0300

    remove api_helper,rb

--- spec/support/api_helper.rb
+++
@@ -1,6 +0,0 @@
-# module ApiHelper
-#   def authenticated_header(request, user)
-#     token = JsonWebToken.encode(user_id: @user.id)
-#     request.headers.merge!('Authorization': "Bearer #{token}")
-#   end
-# end
