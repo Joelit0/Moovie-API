8335b6ed8230a0197f48be4969da4b324daa0149
JoelAlayon123 <joel.alayon@moove-it.com>
2020-06-11 15:09:51 -0300

    Remove repeated time variable

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,6 @@ class AuthenticationController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user&.valid_password?(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
       render json: { token: token,
                     exp: DateTime.current + 24.hours,
                     email: @user.email }, status: :ok
