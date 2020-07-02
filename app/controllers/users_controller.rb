a048da32d2a04573c099c303990cb9edd5728afb
JoelAlayon123 <joel.alayon@moove-it.com>
2020-07-02 16:28:56 -0300

    'add and remove photo path' methods for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -100,9 +100,70 @@ class UsersController < ApplicationController
     end
   end
 
+  def add_photo_path
+    @user = User.find_by(id: params[:id])
+
+    if @user
+      if @user.id == @current_user.id
+        if @user.update_attributes(photo_path_params)
+          render json: { message: "The photo path has been updated correctly", photo_path: @user.photo_path }, status: :ok
+        else
+          render json: { 
+            message: 'The photo path has not been updated'
+          },
+          status: :unprocessable_entity
+        end
+      else
+        render json: { 
+          message: 'You cannot modify other users'
+        },
+        status: :unauthorized
+      end
+    else
+      render json: { 
+        message: 'The user does not exist'
+      },
+      status: :not_found
+    end
+  end
+
+  def remove_photo_path
+    @user = User.find_by(id: params[:id])
+
+    if @user
+      if @user.id == @current_user.id
+        if @user.photo_path == ""
+          render json: { message: "The photo path has already been removed" }, status: :unprocessable_entity
+        else
+          if @user.update_attributes(photo_path: "")
+            render json: { message: "The photo path has been removed correctly" }, status: :ok
+          else
+            render json: { 
+              message: 'The photo path has not been removed'
+            },
+            status: :unprocessable_entity
+          end
+        end
+      else
+        render json: { 
+          message: 'You cannot modify other users'
+        },
+        status: :unauthorized
+      end
+    else
+      render json: { 
+        message: 'The user does not exist'
+      },
+      status: :not_found
+    end
+  end
   private 
 
   def user_params
-    params.permit(:email, :password, :password_confirmation, :full_name, :photo_path)
+    params.permit(:email, :password, :password_confirmation, :full_name)
+  end
+
+  def photo_path_params
+    params.permit(:photo_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :genres
   resources :movies
-  resources :users
+
+  get '/users/:id', to:'users#show'
+  post '/users', to:'users#create'
+  put '/users/:id', to:'users#update'
+  delete'/users/:id', to:'users#destroy'
+  put 'users/:id/photo_path', to:'users#add_photo_path'
+  delete 'users/:id/photo_path', to:'users#remove_photo_path'
+
   
   get '/users/:id/lists', to:'lists#index'
   post '/auth/login', to: 'authentication#login'
