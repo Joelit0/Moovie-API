6ab108459e4f47e55b8d143d69b5136ce7b789c2
JoelAlayon123 <joel.alayon@moove-it.com>
2020-07-02 14:48:32 -0300

    Improve controllers and routes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -121,7 +121,95 @@ class ListsController < ApplicationController
       status: :not_found
     end
   end
+ 
+  def add_movie
+    @movie = Movie.find_by(id: params[:movie_id])
+    @list = List.find_by(id: params[:list_id])
 
+    if @movie
+      if @list
+        if @list.user_id == @current_user.id
+          if @list.movies.include? @movie
+            render json: {
+              message: 'The movie has already been added to this list'
+            },
+            status: :unprocessable_entity
+          else
+            if @list.movies << @movie
+              render json: {
+                message: "The movie has been successfully added to the list", 
+                movie: @movie
+              }, status: :no_content
+            else
+              render json: {
+                message: 'The list could not be updated'
+              },
+              status: :unprocessable_entity
+            end
+          end
+        else
+          render json: { 
+            message: "You cannot add movies to other users' lists"
+          },
+          status: :unauthorized
+        end
+      else
+        render json: { 
+          message: 'The list does not exist'
+        },
+        status: :not_found
+      end
+    else
+      render json: { 
+        message: 'The movie does not exist' 
+      },
+      status: :not_found
+    end
+  end
+
+  def remove_movie
+    @movie = Movie.find_by(id: params[:movie_id])
+    @list = List.find_by(id: params[:list_id])
+
+    if @movie
+      if @list
+        if @list.user_id == @current_user.id
+          if @list.movies.include? @movie
+            if @list.movies.delete(@movie)
+              render json: {
+                message: "The movie has been successfully removed from this list", 
+              }, status: :no_content
+            else
+              render json: {
+                message: 'The movie could not be added to the list'
+              },
+              status: :unprocessable_entity
+            end
+          else
+            render json: { 
+              message: "The movie is not in this list"
+              },
+              status: :unprocessable_entity
+          end
+        else
+          render json: { 
+            message: "You cannot delete movies from other users' lists"
+          },
+          status: :unauthorized
+        end
+      else
+        render json: { 
+          message: 'The list does not exist'
+        },
+        status: :not_found
+      end
+    else
+      render json: { 
+        message: 'The movie does not exist' 
+      },
+      status: :not_found
+    end
+  end
   private
 
   def list_params
